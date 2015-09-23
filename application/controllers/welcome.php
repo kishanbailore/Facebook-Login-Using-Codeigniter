089c55943084ec63d6c5776cb7c798a408bc3783
Kishan K <kishan.bailore@gmail.com>
2015-09-23 23:22:12 +0530

    Facebook Login Using Codeigniter

---
+++ application/controllers/welcome.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+
+        // To use site_url and redirect on this controller.
+        $this->load->helper('url');
+	}
+	public function index()
+	{
+				$this->load->library('facebook'); // Automatically picks appId and secret from config
+        // OR
+        // You can pass different one like this
+        //$this->load->library('facebook', array(
+        //    'appId' => 'APP_ID',
+        //    'secret' => 'SECRET',
+        //    ));
+
+		$user = $this->facebook->getUser();
+        
+        if ($user) {
+            try {
+                $data['user_profile'] = $this->facebook->api('/me?fields=id,first_name,last_name,picture,email');
+            } catch (FacebookApiException $e) {
+                $user = null;
+            }
+        }else {
+            // Solves first time login issue. (Issue: #10)
+            //$this->facebook->destroySession();
+        }
+
+        if ($user) {
+
+            $data['logout_url'] = site_url('welcome/logout'); // Logs off application
+            // OR 
+            // Logs off FB!
+            // $data['logout_url'] = $this->facebook->getLogoutUrl();
+
+        } else {
+            $data['login_url'] = $this->facebook->getLoginUrl(array(
+                'redirect_uri' => site_url('welcome/login'), 
+                'scope' => array("email") // permissions here
+            ));
+        }
+        $this->load->view('login',$data);
+	}
+
+
+    public function logout(){
+
+        $this->load->library('facebook');
+
+        // Logs off session from website
+        $this->facebook->destroySession();
+        // Make sure you destory website session as well.
+
+        redirect('welcome/login');
+    }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
