27dd0981f7c39291584185ad41e6a6e3a060a496
Kishan K <kishan.bailore@gmail.com>
2017-01-02 20:58:35 +0530

    Update facebook.php

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$config['appId']   = '436053676578627';
-$config['secret']  = '1685df2e1a16838eea8838a01effc1a3';
+$config['appId']   = '';
+$config['secret']  = '';
 
 ?>
